70e3a32bbc7031c3dc1045fed9215c0fc504f4b9
Ruslan Kuleshov <ruslan.kuleshov@jetbrains.com>
2023-06-10 20:44:30 +0200

    Working solution. Stage 5 implemented

--- Coffee Machine (Go)/task/main.go
+++ Coffee Machine (Go)/task/main.go
@@ -5,14 +5,15 @@ import (
 )
 
 /* var (
-	availWater       = 400
-	availMilk        = 540
-	availCoffeeBeans = 120
-	availCups        = 9
-	availMoney       = 550
+	availWater       := 400
+	availMilk        := 540
+	availCoffeeBeans := 120
+	availCups        := 9
+	availMoney       := 550
 ) */
 
 func main() {
+
 	// write your code here
 	//Can this block be done better?
 	/*	processStart()
@@ -41,10 +42,34 @@ func main() {
 
 	// ...
 
-	availableResource(&availWater, &availMilk, &availCoffeeBeans, &availCups, &availMoney)
+	for {
+		//availableResource(&availWater, &availMilk, &availCoffeeBeans, &availCups, &availMoney)
+
+		var action string
+		fmt.Println("Write action (buy, fill, take, remaining, exit):")
+		fmt.Scan(&action)
+
+		switch action {
+		case "buy":
+			buyCoffee(&availWater, &availMilk, &availCoffeeBeans, &availCups, &availMoney)
+		case "fill":
+			fillResources(&availWater, &availMilk, &availCoffeeBeans, &availCups)
+		case "take":
+			takeMoney(&availMoney)
+		case "remaining":
+			availableResource(&availWater, &availMilk, &availCoffeeBeans, &availCups, &availMoney)
+		case "exit":
+			return
+		default:
+			fmt.Println("Invalid action. Please try again.")
+		}
+
+	}
+
+	/*availableResource(&availWater, &availMilk, &availCoffeeBeans, &availCups, &availMoney)
 	requestAction(&availWater, &availMilk, &availCoffeeBeans, &availCups, &availMoney)
 	availableResource(&availWater, &availMilk, &availCoffeeBeans, &availCups, &availMoney)
-
+	*/
 }
 
 func availableResource(availWater, availMilk, availCoffeeBeans, availCups, availMoney *int) {
@@ -55,6 +80,7 @@ func availableResource(availWater, availMilk, availCoffeeBeans, availCups, avail
 	fmt.Printf("%d g of coffee beans\n", *availCoffeeBeans)
 	fmt.Printf("%d disposable cups\n", *availCups)
 	fmt.Printf("$%d of money \n", *availMoney)
+	fmt.Println()
 
 	//Can I put the  block below into separate function?
 	/*	fmt.Println("Write how many ml of water the coffee machine has:")
@@ -96,7 +122,73 @@ func availableResource(availWater, availMilk, availCoffeeBeans, availCups, avail
 	*/
 }
 
-func requestAction(availWater, availMilk, availCoffeeBeans, availCups, availMoney *int) {
+func buyCoffee(availWater, availMilk, availCoffeeBeans, availCups, availMoney *int) {
+	var response string
+	fmt.Println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu:")
+	fmt.Scan(&response)
+
+	if response == "1" {
+		makeCoffee(250, 0, 16, 4, availWater, availMilk, availCoffeeBeans, availCups, availMoney)
+	} else if response == "2" {
+		makeCoffee(350, 75, 20, 7, availWater, availMilk, availCoffeeBeans, availCups, availMoney)
+	} else if response == "3" {
+		makeCoffee(200, 100, 12, 6, availWater, availMilk, availCoffeeBeans, availCups, availMoney)
+	} else if response == "back" {
+		return
+	} else {
+		fmt.Println("Invalid option. Please try again.")
+	}
+}
+
+func makeCoffee(water, milk, coffeeBeans, cost int, availWater, availMilk, availCoffeeBeans, availCups, availMoney *int) {
+	if *availWater < water {
+		fmt.Println("Sorry, not enough water!")
+		return
+	}
+	if *availMilk < milk {
+		fmt.Println("Sorry, not enough milk!")
+		return
+	}
+	if *availCoffeeBeans < coffeeBeans {
+		fmt.Println("Sorry, not enough coffee beans!")
+		return
+	}
+	if *availCups < 1 {
+		fmt.Println("Sorry, not enough disposable cups!")
+		return
+	}
+
+	fmt.Println("I have enough resources, making you a coffee!")
+	*availWater -= water
+	*availMilk -= milk
+	*availCoffeeBeans -= coffeeBeans
+	*availMoney += cost
+	*availCups -= 1
+}
+
+func fillResources(availWater, availMilk, availCoffeeBeans, availCups *int) {
+	var addWater, addMilk, addCoffeeBeans, addCups int
+	fmt.Println("Write how many ml of water you want to add:")
+	fmt.Scan(&addWater)
+	fmt.Println("Write how many ml of milk you want to add:")
+	fmt.Scan(&addMilk)
+	fmt.Println("Write how many grams of coffee beans you want to add:")
+	fmt.Scan(&addCoffeeBeans)
+	fmt.Println("Write how many disposable cups you want to add:")
+	fmt.Scan(&addCups)
+
+	*availWater += addWater
+	*availMilk += addMilk
+	*availCoffeeBeans += addCoffeeBeans
+	*availCups += addCups
+}
+
+func takeMoney(availMoney *int) {
+	fmt.Printf("I gave you $%d\n", *availMoney)
+	*availMoney = 0
+}
+
+/*func requestAction(availWater, availMilk, availCoffeeBeans, availCups, availMoney *int) {
 	var action string
 	fmt.Println("Write action (buy, fill, take):")
 	fmt.Scan(&action)
@@ -151,8 +243,9 @@ func requestAction(availWater, availMilk, availCoffeeBeans, availCups, availMone
 
 	}
 
-}
-func waterCalculation(cupsAmount int) int {
+}*/
+
+/*func waterCalculation(cupsAmount int) int {
 	return cupsAmount * 200
 }
 
@@ -185,3 +278,4 @@ func pouringMilk() {
 func readyMessage() {
 	fmt.Println("Coffee is ready!")
 }
+*/
